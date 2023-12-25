f290735ac52f68943f0ae7d487b4d463b400a5a2
seodongsu <xl445566@naver.com>
2023-12-25 23:39:11 +0900

    FIX: 모듈경로 오류

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,7 @@
+import '@/app/ui/global.css'; // Tip: @는 프로젝트의 root를 의미
+import { inter } from '@/app/ui/fonts';
+
+// Tip: layout파일을 이용해 레이아웃을 공유할 수 있다.
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +9,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Lusitana } from 'next/font/google';
+
+// Tip: 글꼴 적용
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
